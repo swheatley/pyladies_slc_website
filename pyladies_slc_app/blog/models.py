590f44cf356ba0402098a7de2543b7b0291799bc
swheatley <shaylee1@me.com>
2017-01-23 14:31:51 -0700

    Added blog functionality

--- pyladies_slc_app/blog/models.py
+++ pyladies_slc_app/blog/models.py
@@ -5,11 +5,35 @@ from django.db import models
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailsearch import index
 
 
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
 
+    def get_context(self, request):
+        context = super(BlogIndexPage, self).get_context(request)
+        blogpages = self.get_children().live().order_by('-first_published_at')
+        context['blogpages'] = blogpages
+        return context
+
     content_panels = Page.content_panels + [
         FieldPanel('intro', classname="full")
+    ]
+
+
+class BlogPage(Page):
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('intro'),
+        FieldPanel('body', classname="full"),
     ]
